685eae3a31d2494827eceba1d2f3c5b0419231a3
arjenlgill <arjenlgill@gmail.com>
2021-05-01 21:31:43 +0100

    passing data from child to parent for reusability

--- src/components/Single.vue
+++ src/components/Single.vue
@@ -1,10 +1,9 @@
 <template>
-  <div v-bind:key="post" v-for="post in post" id="post-wrapper">
+<div v-bind:key="post" v-for="post in post">
+  <div id="post-wrapper">
     <div class="post-img-container">
       <img
-        v-bind:key="post"
-        v-on:click="lyricsConcat"
-        :src="post.img"
+        :src="img"
         alt=""
         class="post-img"
       />
@@ -12,17 +11,19 @@
 
     <div class="post-content-container">
       <div id="tte">
-        <h1 class="post-h1">{{ post.postH1 }}</h1>
+        <h1 v-if="postH1" class="post-h1">{{ postH1 }}</h1>
 
-        <h2 class="post-h2">{{ post.postH2 }}</h2>
+        <h2 class="post-h2">{{ postH2 }}</h2>
 
-        <!--<div v-bind:key="item" v-for="item in post.lyricsOG" class="lyrics-og">
+        <p class="post-intro">{{ postIntro }}</p>
+
+        <!--<div v-bind:key="item" v-for="item in lyricsOG" class="lyrics-og">
         
         <p>{{item}}</p>
 
-        <p v-bind:key="item" v-for="item in post.lyricsTrans" class="lyrics-trans">{{item}}</p>
+        <p v-bind:key="item" v-for="item in lyricsTrans" class="lyrics-trans">{{item}}</p>
 
-      </div> -->
+        </div> -->
 
         <div v-bind:key="lyric" v-for="(lyric, index) in lyrics">
           <p v-if="index % 2 === 0" class="lyrics-og">
@@ -32,39 +33,43 @@
           <p v-else class="lyrics-trans">{{ lyric }}</p>
         </div>
 
-        <p class="post-content">{{ post.postContent }}</p>
+        <p class="post-content">{{ postContent }}</p>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data () {
     return {
       post: [
         {
           img: require("../assets/img/amantejhundal.jpg"),
-          postH1: "AMANTEJ HUNDAL X SIGNATURE BY SB: 'NAZAARE'",
-          postH2: "Chorus",
+          postH1: "sample",
+          postH2: "sample",
+          postIntro: "",
           lyricsOG: [
-            "jatt ne tah zindagi che lehne aa nazaare", "testOG"
+            "sample", "sample"
           ],
           lyricsTrans: [
-            "test2", "test3"
+            "sample", "sample"
           ],
           postContent: "",
         },
       ],
     };
   },
-  computed: {
-    lyrics() {
-      let mo = this.post[0].lyricsOG.map((x, y) => {
-        return [x, this.post[0].lyricsTrans[y]];
-      });
-      return mo[0].concat(mo[1]);
-    },
+  props: {
+    img: String,
+    postH1: String,
+    postH2: String,
+    postIntro: String,
+    lyricsOG: Array,
+    lyricsTrans: Array,
+    postContent: String,
+    lyrics: Array
   },
 };
 </script>
--- src/subpages/singleone/App.vue
+++ src/subpages/singleone/App.vue
@@ -1,30 +1,55 @@
 <template>
   <MobNav />
-  <Single />
+  <Single
+    :img="this.post.img"
+    :postH1="this.post.postH1"
+    :postH2="this.post.postH2"
+    :postIntro="this.post.postIntro"
+    :lyricsOG="this.post.lyricsOG"
+    :lyricsTrans="this.post.lyricsTrans"
+    :postContent="this.post.postContent"
+    v-bind:lyrics="lyrics"
+  />
   <MoreMusic />
   <Footer />
-
 </template>
 
 <script>
-import MobNav from '../../components/MobNav.vue'
-import Single from '../../components/Single.vue'
-import MoreMusic from '../../components/MoreMusic.vue'
-import Footer from '../../components/Footer.vue'
-import '../../assets/scss/style.scss'
+import MobNav from "../../components/MobNav.vue";
+import Single from "../../components/Single.vue";
+import MoreMusic from "../../components/MoreMusic.vue";
+import Footer from "../../components/Footer.vue";
+import "../../assets/scss/style.scss";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     MobNav,
     Single,
     MoreMusic,
-    Footer
+    Footer,
   },
+  
   data() {
     return {
-    
+      post: {
+        img: require("../../assets/img/amantejhundal.jpg"),
+        postH1: "AMANTEJ HUNDAL X SIGNATURE BY SB: 'test'",
+        postH2: "TestChros",
+        postIntro: "",
+        lyricsOG: ["jatt ne tah zindagi che lehne aa nazaare", "testOG"],
+        lyricsTrans: ["test2", "test3"],
+        postContent: "",
       }
-    }
-}
+    };
+  },
+    computed: {
+    lyrics() {
+      let mo = this.post.lyricsOG.map((x, y) => {
+        return [x, this.post.lyricsTrans[y]];
+      });
+      return mo[0].concat(mo[1]);
+    },
+  },
+};
 </script>
